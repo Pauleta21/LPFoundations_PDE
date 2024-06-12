fd705baa43bcb37fbca1b4d557c8ddb52c606cf0
Pauleta21 <you@example.com>
2024-06-12 18:47:11 +0200

    Refactoring cleaning.py file

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,37 +1,42 @@
-import argparse
 import os
 import pandas as pd
 import numpy as np
 
+def load_data():
+    initial_data = pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv", sep='\t', header=0)
+    return initial_data
 
-def clean_data(country):
+def clean_data(initial_data):
+    initial_data[['unit', 'sex', 'age','region']] = initial_data['unit,sex,age,geo\\time'].str.split(',', expand=True)
+    initial_data = initial_data.drop(['unit,sex,age,geo\\time'], axis=1)
 
-    data=pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv",sep='\t', header=0)
-
-    data[['unit', 'sex', 'age','region']]=data['unit,sex,age,geo\\time'].str.split(',', expand=True)
-    data = data.drop(['unit,sex,age,geo\\time'], axis=1)
-
-    var_columns = data.iloc[:, 62:66]
-    var_values = data.iloc[:, :62]
-    data_long = pd.melt(data, id_vars=var_columns, value_vars=var_values,
+    var_columns = initial_data.iloc[:, 62:66]
+    var_values = initial_data.iloc[:, :62]
+    data_clean = pd.melt(initial_data, id_vars=var_columns, value_vars=var_values,
                         var_name='year', value_name='value')
 
-    data_long['year'].replace([np.inf, -np.inf], np.nan, inplace=True)
-    data_long.dropna(subset=['year'], inplace=True)
+    data_clean['year'].replace([np.inf, -np.inf], np.nan, inplace=True)
+    data_clean.dropna(subset=['year'], inplace=True)
+    data_clean['value'] = data_clean['value'].str.replace('e', '')
+    data_clean = data_clean[data_clean['year'] != 'unit']
 
-    data_long['value'] = data_long['value'].str.replace('e', '')
-    data_long = data_long[data_long['year'] != 'unit']
+    data_clean['year'] = pd.to_numeric(data_clean['year'], errors='coerce').astype('int')
+    data_clean['value'] = pd.to_numeric(data_clean['value'], errors='coerce').astype('float')
+    data_clean.dropna(subset=['value'], inplace=True)
 
-    data_long['year'] = pd.to_numeric(data_long['year'], errors='coerce').astype('int')
-    data_long['value'] = pd.to_numeric(data_long['value'], errors='coerce').astype('float')
-    data_long.dropna(subset=['value'], inplace=True)
+    data_pt = data_clean[data_clean['region'] == 'PT']
 
-    data_pt = data_long[data_long['region'] == 'PT']
+    return data_pt
+
+def save_data(data_pt):
     new_directory = os.path.join('life_expectancy/data', 'pt_life_expectancy.csv')
     data_pt.to_csv(new_directory, index=False)
 
-if __name__ == "__main__":  # pragma: no cover
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--country", default="PT", help="Country code to filter the data")
-    args = parser.parse_args()
-    clean_data(args.country)
+def main_function():
+    initial_data = load_data()
+    if initial_data is not None:
+        data_pt = clean_data(initial_data)
+        save_data(data_pt)
+
+if __name__ == "__main__":
+    main_function()
